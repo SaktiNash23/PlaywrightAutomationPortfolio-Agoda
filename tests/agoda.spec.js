de0cf6284804aea8d793ee77b57aefa711990c42
Sativel Thamilvanan <sativel@hotmail.com>
2026-01-24 20:45:35 +0800

    Initial Commit - PlaywrightAutomation_Agoda

---
+++ tests/agoda.spec.js
@@ -0,0 +1,14 @@
+/*TEST PLAN
+
+https://www.agoda.com/
+
+1. Membership Registration
+2. Login & Logout
+3. Logged In E2E tests (Hotels Reservation)
+4. Language Select
+5. Currency Select
+6. Add a Hotel to Favourites
+
+*/
+const {test, expect} = require('@playwright/test');
+
