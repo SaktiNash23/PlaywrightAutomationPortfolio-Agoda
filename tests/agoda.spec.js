e87e052a067f87b2ad1e9fc23588b6d425d1b02a
Sativel Thamilvanan <sativel@hotmail.com>
2026-02-22 20:59:49 +0800

    22 Februaruy 2026

--- tests/agoda.spec.js
+++ tests/agoda.spec.js
@@ -15,6 +15,8 @@ const {test, expect} = require('@playwright/test');
 
 test.only("Agoda - Membership Registration", async ({browser})=>
 {
+    const email = ""
+    
     const context = await browser.newContext()
     const page = await context.newPage()
     await page.goto("https://www.agoda.com/")//Open site
@@ -22,51 +24,14 @@ test.only("Agoda - Membership Registration", async ({browser})=>
     await expect(agodaLogo).toBeVisible()//Wait until the page is loaded by checking if the "Agoda" logo is loaded
 
     await page.getByRole("button", {name: "Create Account"}).click()//Click Create Account
-
-    //Sign In/Login page contains an iframe, so we need to locate the iframe using frameLocator first
-    const iframePage = page.frameLocator("iframe[title='Universal login']")
+    const iframePage = page.frameLocator("iframe[title='Universal login']")//Sign In/Login page contains an iframe, so we need to locate the iframe using frameLocator first
     await expect(iframePage.locator("h2.sc-fvtFIe")).toContainText("Sign in or create an account")//Check if the "Create Account/Login" screen is loaded
-
-
-    //Go to InstAddr and login to retrieve the OTP
-    
-    const InstAddr = "https://m.kuku.lu/recv.php"
-    const InstAddr_AccountID = "254884629175"
-    const InstAddr_Pasword = "wJVQ#:bu0FIAna"
-    const email = "oakantday@digdig.org"
-    //const email = "payink7@otona.uk"
-
-
-    const page2 = await page.context().newPage()
-    await page2.goto(InstAddr)
-
-    await page2.getByText("Config").click()
-    await page2.locator("a[data-icon='power']").click()
-    await page2.locator("#link_loginform").click()
-    await page2.locator("#user_number").fill(InstAddr_AccountID)
-    await page2.locator("#user_password").fill(InstAddr_Pasword)
-    await page2.locator("a[href*='checkLogin']").click()
-    await page2.locator("#area-confirm-dialog-button-ok").click()
-
-    await page2.getByText(email).click()
-    await page2.locator("a[data-icon='mail']").click()
-    await page2.locator("a[id*='link_maildata']").first().click()
-
-    const otpEmailHeader = await page2.locator("div.full").textContent()
-    const emailHeaderArr = otpEmailHeader.split("")
-    const otp = emailHeaderArr[-1]
-    console.log("OTP: " + otp)
-    
-    await page2.pause()
-    
-
 
     await expect(iframePage.locator("input[placeholder='id@email.com']")).toBeEnabled()
     await iframePage.locator("input[placeholder='id@email.com']").fill(email)
     await expect(iframePage.locator("button[data-cy='unified-email-continue-button']")).toBeEnabled()
     await iframePage.locator("button[data-cy='unified-email-continue-button']").click()
 
-    /*
     const firstName = "Sativel"
     const lastName = "Nathan"
     await iframePage.locator("input[autocomplete='given-name']").fill(firstName)
@@ -75,17 +40,44 @@ test.only("Agoda - Membership Registration", async ({browser})=>
     await expect(iframePage.getByRole("checkbox")).not.toBeChecked()
     await expect(iframePage.locator("button[data-cy='profile-continue-button']")).toBeEnabled()
     await iframePage.locator("button[data-cy='profile-continue-button']").click()
-    */
+
+
+    //Go to InstAddr and login to retrieve the OTP
+    const InstAddr = "https://m.kuku.lu/recv.php"
+    const InstAddr_AccountID = "254884629175"
+    const InstAddr_Pasword = "wJVQ#:bu0FIAna"
+
+
+    const page2 = await page.context().newPage()
+    await page2.goto(InstAddr)
+    await page2.getByText("Config").click()
+    await page2.locator("a[data-icon='power']").click()
+    await page2.locator("#link_loginform").click()
+    await page2.locator("#user_number").fill(InstAddr_AccountID)
+    await page2.locator("#user_password").fill(InstAddr_Pasword)
+    await page2.locator("a[href*='checkLogin']").click()
+    await page2.locator("#area-confirm-dialog-button-ok").click()
+    await page2.getByText(email).click()
+    await page2.locator("a[data-icon='mail']").click()
+    let emailContent = await page2.locator("a[id*='link_maildata']").first().textContent()
+    console.log(emailContent)
+
+    //RETRIEVE THE OTP FROM THE EMAIL HEADER
+    const emailHeaderArr1 = emailContent.split("is")
+    const otp = emailHeaderArr1[1].trim()
 
     await expect(iframePage.locator("div[data-cy='form-heading']")).toContainText("Sign in with OTP")
-    await iframePage.locator("input[name='otp-0']").pressSequentially("123")//This works
+
+    for(i = 0; i < otp.length < i++;)
+    {
+        await iframePage.locator(`input[name='otp-${i}']`).pressSequentially(otp[i])//This works
+    }
+
+    //await iframePage.locator("input[name='otp-0']").pressSequentially(otp[])//This works
+    await iframePage.locator("button[data-cy='unified-auth-otp-continue-button']").click()
     await page.pause()
-
-    //const otpBox = page.locator("div.ad15b-mt-16").first()
-    //await otpBox.fill()
-
-
-
+    
+    
 });
 
 
@@ -136,3 +128,26 @@ test("Agoda - Language & Currency Select", async({browser, page})=>
     await page2.pause()
 
 });
+
+
+//CODEGEN CODE
+/*
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://m.kuku.lu/recv.php');
+  await page.getByRole('img').nth(3).click();
+  await page.getByRole('button', { name: 'Account' }).click();
+  await page.getByRole('button', { name: 'Sign in to another account' }).click();
+  await page.getByRole('textbox', { name: 'AccountID' }).click();
+  await page.getByRole('textbox', { name: 'AccountID' }).fill('254884629175');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('wJVQ#:bu0FIAna');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('button', { name: 'Yes' }).click();
+  await page.getByRole('link').filter({ hasText: /^$/ }).nth(4).click();
+  await page.getByRole('button', { name: 'Your email OTP for Agoda is 327563 14:16 (20day) | Agoda <no-reply@account.' }).click();
+  await page.locator('iframe[name="area_maildata_iframe_42604354"]').contentFrame().getByRole('cell', { name: 'Hi, your Agoda OTP is 327563. It\'s valid for 10 minutes.', exact: true }).click();
+});
+
+*/
