d2e1cb8759c0265865031ffe42df574b614d411e
Sativel Thamilvanan <sativel@hotmail.com>
2026-03-14 16:07:03 +0800

    Improved Membership Registration flow - Self create fresh email

--- tests/agoda.spec.js
+++ tests/agoda.spec.js
@@ -16,12 +16,17 @@ const {test, expect} = require('@playwright/test');
 const user = 
 {
     loginEmail: "sativel@hotmail.com",
-    loginPassword: "Newpassword1",
-    userName: "Sativel N."
+    loginPassword: "Newpassword2",
+    userName: "Sativel N.",
+    firstName: "Sativel",
+    lastName: "Nathan",
+    InstAddrURL: "https://m.kuku.lu/recv.php",
+    InstAddr_AccountID: "254884629175",
+    InstAddr_Password: "wJVQ#:bu0FIAna"
 
 }
 
-test.only("Agoda - Login - Created Account with successful first-time Login", async ({page})=>
+test("Agoda - Login - Created Account with successful first-time Login", async ({page})=>
 {
     await page.goto("https://www.agoda.com/")
     const agodaLogo = page.locator(".cxsKGP")
@@ -43,79 +48,78 @@ test.only("Agoda - Login - Created Account with successful first-time Login", as
 
 //test.use({ storageState: 'auth.json' });
 //TODO: It works up until the part where we need to click "Not Now" to refuse passkey setup. But this code can be improved I think
-test("Agoda - Membership Registration", async ({browser})=>
+test.only("Agoda - Membership Registration", async ({browser})=>
 {
-    const email = "toefix648@pngk.uk"
-    
-    const context = await browser.newContext()
-    const page = await context.newPage()
-    await page.goto("https://www.agoda.com/")//Open site
-    const agodaLogo = page.locator(".cxsKGP")
-    await expect(agodaLogo).toBeVisible()//Wait until the page is loaded by checking if the "Agoda" logo is loaded
+    //CREATE A FRESH EMAIL ADDRESS
+    const context1 = await browser.newContext()
+    const page1 = await context1.newPage()
+    await page1.goto(user.InstAddrURL)
+    await page1.getByText("Config").click()
+    await page1.locator("a[data-icon='power']").click()
+    await page1.locator("#link_loginform").click()
+    await page1.locator("#user_number").fill(user.InstAddr_AccountID)
+    await page1.locator("#user_password").fill(user.InstAddr_Password)
+    await page1.locator("a[href*='checkLogin']").click()
+    await page1.locator("#area-confirm-dialog-button-ok").click()
+    await page1.locator("#link_addMailAddrByOnetime").click()
+    const freshEmail = await page1.locator("div.noticebox u b").first().textContent()
+    console.log("freshEmail: "+ freshEmail)
 
-    await page.getByRole("button", {name: "Create Account"}).click()//Click Create Account
-    const iframePage = page.frameLocator("iframe[title='Universal login']")//Sign In/Login page contains an iframe, so we need to locate the iframe using frameLocator first
-    await expect(iframePage.locator("h2.sc-fvtFIe")).toContainText("Sign in or create an account")//Check if the "Create Account/Login" screen is loaded
+
+    //GO TO AGODA AND START THE MEMBERSHIP REGISTRATION PROCESS
+    const context2 = await browser.newContext()
+    const page2 = await context2.newPage()
+    await page2.goto("https://www.agoda.com/")
+    const agodaLogo = page2.locator(".cxsKGP")
+    await expect(agodaLogo).toBeVisible()
+
+    await page2.getByRole("button", {name: "Create Account"}).click()
+    const iframePage = page2.frameLocator("iframe[title='Universal login']")//Sign In/Login page contains an iframe, so we need to locate the iframe using frameLocator first
+    await expect(iframePage.locator("h2.sc-fvtFIe")).toContainText("Sign in or create an account")
 
     await expect(iframePage.locator("input[placeholder='id@email.com']")).toBeEnabled()
-    await iframePage.locator("input[placeholder='id@email.com']").fill(email)
+    await iframePage.locator("input[placeholder='id@email.com']").fill(freshEmail)
     await expect(iframePage.locator("button[data-cy='unified-email-continue-button']")).toBeEnabled()
     await iframePage.locator("button[data-cy='unified-email-continue-button']").click()
 
-    const firstName = "Sativel"
-    const lastName = "Nathan"
-    await iframePage.locator("input[autocomplete='given-name']").fill(firstName)
-    await iframePage.locator("input[autocomplete='family-name']").fill(lastName)
+    await iframePage.locator("input[autocomplete='given-name']").fill(user.firstName)
+    await iframePage.locator("input[autocomplete='family-name']").fill(user.lastName)
     await iframePage.getByRole("checkbox").click()
     await expect(iframePage.getByRole("checkbox")).not.toBeChecked()
     await expect(iframePage.locator("button[data-cy='profile-continue-button']")).toBeEnabled()
     await iframePage.locator("button[data-cy='profile-continue-button']").click()
 
-
-    //Go to InstAddr and login to retrieve the OTP
-    const InstAddr = "https://m.kuku.lu/recv.php"
-    const InstAddr_AccountID = "254884629175"
-    const InstAddr_Pasword = "wJVQ#:bu0FIAna"
-
-    const page2 = await page.context().newPage()
-    await page2.goto(InstAddr)
-
-    //FRESH INSTADDR LOGIN
-    
-    await page2.getByText("Config").click()
-    await page2.locator("a[data-icon='power']").click()
-    await page2.locator("#link_loginform").click()
-    await page2.locator("#user_number").fill(InstAddr_AccountID)
-    await page2.locator("#user_password").fill(InstAddr_Pasword)
-    await page2.locator("a[href*='checkLogin']").click()
-    await page2.locator("#area-confirm-dialog-button-ok").click()
-    await page2.getByText(email).click()
-    await page2.locator("a[data-icon='mail']").click()
-    let emailContent = await page2.locator("div[id*='area_mail_title_']").first().textContent()
+    //GET THE EMAIL FROM INSTADDR
+    const context3 = await browser.newContext()
+    const page3 = await context3.newPage()
+    await page3.goto(user.InstAddrURL)
+    await page3.getByText("Config").click()
+    await page3.locator("a[data-icon='power']").click()
+    await page3.locator("#link_loginform").click()
+    await page3.locator("#user_number").fill(user.InstAddr_AccountID)
+    await page3.locator("#user_password").fill(user.InstAddr_Password)
+    await page3.locator("a[href*='checkLogin']").click()
+    await page3.locator("#area-confirm-dialog-button-ok").click()
+    await page3.getByText(freshEmail).click()
+    await page3.locator("a[data-icon='mail']").click()
+    let emailContent = await page3.locator("div[id*='area_mail_title_']").first().textContent()
     
 
-    //INSTADDR WITH AUTH.JSON LOGIN (Remembers Credentials)
+    
     /*
+    //INSTADDR WITH AUTH.JSON LOGIN (Remembers Credentials)
     await page2.locator("#search-basic").fill(email)
     await page2.press('#search-basic', 'Enter');
     let emailContent = await page2.locator("div[id*='area_mail_title_']").first().textContent()
+    await context.storageState({ path: 'auth.json' });//Save login credentials of InstAddr?
     */
 
-
-    console.log(emailContent)
-
-    await context.storageState({ path: 'auth.json' });//Save login credentials of InstAddr?
 
     //RETRIEVE THE OTP FROM THE EMAIL HEADER
     const emailHeaderArr1 = emailContent.split("is")
     const otpStr = emailHeaderArr1[1].trim()
     const otp = parseInt(otpStr)
-
     let arrNums = otpStr.toString().split('').map(Number);
-    console.log("arrNums: "+arrNums);
-
-    console.log("OTP after trim:"+otp)
-    console.log("OTP length:"+otpStr.length)
 
     await expect(iframePage.locator("div[data-cy='form-heading']")).toContainText("Sign in with OTP")
     await iframePage.locator("input[name='otp-0']").pressSequentially(otpStr[0], {delay:150})
@@ -126,7 +130,9 @@ test("Agoda - Membership Registration", async ({browser})=>
     await iframePage.locator("input[name='otp-5']").pressSequentially(otpStr[5], {delay:150})
     await iframePage.locator("button[data-cy='unified-auth-otp-continue-button']").click()
 
-    await page.pause()
+    await page2.pause()
+    
+    
     
 });
 
@@ -139,9 +145,9 @@ test("Agoda - Language & Currency Select", async({browser, page})=>
     const languageCode = "jp"//Use Inspector to check for the lang variable for the country code
     const homepageHeaderText = "もっとおトクに世界を旅しよう"//Check the Homepage header text for the value
 
-    await page.goto("https://www.agoda.com/")//Open site
+    await page.goto("https://www.agoda.com/")
     const agodaLogo = page.locator(".cxsKGP")
-    await expect(agodaLogo).toBeVisible()//Wait until the page is loaded by checking if the "Agoda" logo is loaded
+    await expect(agodaLogo).toBeVisible()
 
     await page.locator(".Flag__StyledCdnIcon-sc-nifs7d-0").click()
     await page.locator(`p[lang='${languageCode}']`).click()
@@ -160,7 +166,6 @@ test("Agoda - Language & Currency Select", async({browser, page})=>
     await page.locator("#textInput").pressSequentially("バンコク", {delay: 500})
 
     await page.getByRole('option', { name: 'バンコク, タイ' }).first().click();//TODO: Update this to be more generic
-    //await page.locator("ul li.Suggestion").first().click()
 
     await page.locator("#full-tab-2").click()//I'm flexible tab
     await page.locator("button[data-testid='flexible-dates-select-button']").click()
@@ -174,7 +179,6 @@ test("Agoda - Language & Currency Select", async({browser, page})=>
     ])
     
     
-    //await expect(page.locator("#price-filter-group").first()).toBeVisible()
     await expect(page2.locator("span.PropertyCardPrice__Currency").nth(1)).toContainText(currencyIcon)
     await page2.pause()
 
